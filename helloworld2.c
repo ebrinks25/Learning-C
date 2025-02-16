d5aaed27ad98c3a9e016a928de63c2e65e0dbf45
Ethan Brinks <56314607+ebrinks25@users.noreply.github.com>
2025-02-16 12:18:25 -0500

    Create helloworld2.c

---
+++ helloworld2.c
@@ -0,0 +1,22 @@
+/*
+* helloworld2.c
+* My second C program
+* by Ethan
+* created 02-16-2025
+*/
+
+#include <stdio.h>
+
+void printComma()
+{
+    printf(", ");
+    return;
+}
+
+int main()
+{
+    printf("Hello");
+    printComma();
+    printf("world!\n");
+    return 0;
+}
